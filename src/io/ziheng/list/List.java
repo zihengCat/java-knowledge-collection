6b1482f1f0c9d87ec65942d6c88afd45f26e131b
zihengCat <zihenglv@gmail.com>
2020-01-30 20:21:46 +0800

    [UPDATE]: List.java

--- src/io/ziheng/list/List.java
+++ src/io/ziheng/list/List.java
@@ -3,13 +3,16 @@ public interface List<E> {
     int size();
     boolean isEmpty();
     boolean contains(E element);
-    void add(E element);
-    void remove(E element);
+    int indexOf(E element);
     void clear();
+    void addFirst(E element);
+    void addLast(E element);
+    void add(int index, E element);
+    E removeFirst();
+    E removeLast();
+    E remove(int index);
     E get(int index);
     E set(int index, E element);
-    void add(int index, E element);
-    E remove(int index);
-    int indexOf(E element);
+    String toString();
 }
 /* EOF */
