290a0293a1451c6a8d80887265c8cb567aa14008
zihengCat <zihenglv@gmail.com>
2020-02-13 03:14:01 +0800

    [UPDATE]: KnapsackProblem.java

--- src/io/ziheng/recursion/dynamicprogramming/KnapsackProblem.java
+++ src/io/ziheng/recursion/dynamicprogramming/KnapsackProblem.java
@@ -11,9 +11,52 @@ public class KnapsackProblem {
             {2, 10},
             {3, 12},
         };
+        int capacity = 5;
+        KnapsackProblem knapsackProblem = new KnapsackProblem();
         System.out.println(
-            new KnapsackProblem().knapsackProblem(items, 5)
+            knapsackProblem.knapsackProblem(items, capacity)
         );
+        System.out.println(
+            knapsackProblem.knapsackProblemV2(items, capacity)
+        );
+    }
+
+    /**
+     * int[index][capacity] = maxValue
+     */
+    private int[][] memory;
+    public int knapsackProblemV2(int[][] items, int capacity) {
+        memory = new int[items.length + 1][capacity + 1];
+        for (int[] row : memory) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = -1;
+            }
+        }
+        return findMaxValueV2(items, items.length - 1, capacity);
+    }
+    private int findMaxValueV2(int[][] items, int index, int capacity) {
+        /**
+         * 递归退出条件：
+         * 没有物品可取 or 背包空间不足 -> 价值为 0
+         */
+        if (index < 0 || capacity <= 0) {
+            return 0;
+        }
+        if (memory[index][capacity] != -1) {
+            return memory[index][capacity];
+        }
+        int maxValueA = -1;
+        int maxValueB = -1;
+        maxValueA = findMaxValueV2(items, index - 1, capacity);
+        if (capacity >= items[index][0]) {
+            maxValueB = items[index][1] + findMaxValueV2(
+                items,
+                index - 1,
+                capacity - items[index][0]
+            );
+        }
+        memory[index][capacity] = Math.max(maxValueA, maxValueB);
+        return memory[index][capacity];
     }
     /**
      * 背包问题。
@@ -41,8 +84,8 @@ public class KnapsackProblem {
         if (index < 0 || capacity <= 0) {
             return 0;
         }
-        int maxValueA = 0;
-        int maxValueB = 0;
+        int maxValueA = -1;
+        int maxValueB = -1;
         maxValueA = findMaxValue(items, index - 1, capacity);
         if (capacity >= items[index][0]) {
             maxValueB = items[index][1] + findMaxValue(
