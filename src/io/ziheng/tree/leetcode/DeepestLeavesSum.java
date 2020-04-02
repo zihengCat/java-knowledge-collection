f94ad7a08483d2dd9f17423c96e0c96291381ddb
zihengCat <zihenglv@gmail.com>
2020-04-02 15:46:52 +0800

    [UPDATE]: LC

---
+++ src/io/ziheng/tree/leetcode/DeepestLeavesSum.java
@@ -0,0 +1,14 @@
+package io.ziheng.tree.leetcode;
+
+import io.ziheng.tree.leetcode.TreeNode;
+
+/**
+ * LeetCode 1302. Deepest Leaves Sum
+ * https://leetcode.com/problems/deepest-leaves-sum/
+ */
+public class DeepestLeavesSum {
+    public int deepestLeavesSum(TreeNode root) {
+        return 0;
+    }
+}
+/* EOF */
