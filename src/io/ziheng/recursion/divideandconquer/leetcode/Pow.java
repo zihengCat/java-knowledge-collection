65abbe78d1eb6b4027f818c373ccfd4b5a1c12bf
zihengCat <zihenglv@gmail.com>
2020-07-19 12:28:32 +0800

    Update LeetCode

--- src/io/ziheng/recursion/divideandconquer/leetcode/Pow.java
+++ src/io/ziheng/recursion/divideandconquer/leetcode/Pow.java
@@ -7,20 +7,24 @@ public class Pow {
     public double myPow(double x, int n) {
         if (n < 0) {
             return 1 / powDivideAndConquer(x, n);
-        } else {
-            return powDivideAndConquer(x, n);
         }
+        return powDivideAndConquer(x, n);
+    }
+    private double powBruteForce(double x, int n) {
+        double r = 1;
+        for (; n > 0; n--) {
+            r = r * x;
+        }
+        return r;
     }
     private double powDivideAndConquer(double x, int n) {
-        // 终止条件
         if (n == 0) {
             return 1;
         }
-        // 处理逻辑
         double halfResult = powDivideAndConquer(x, n / 2);
         return n % 2 == 0
-        ? halfResult * halfResult
-        : halfResult * halfResult * x;
+            ? halfResult * halfResult
+            : halfResult * halfResult * x;
     }
 }
 /* EOF */
