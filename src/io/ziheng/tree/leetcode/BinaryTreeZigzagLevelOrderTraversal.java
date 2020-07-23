4dad3478a2bd1507aac62410323f08579230bac2
zihengCat <zihenglv@gmail.com>
2020-07-23 14:21:16 +0800

    Update LC

--- src/io/ziheng/tree/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/io/ziheng/tree/leetcode/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,47 +1,68 @@
 package io.ziheng.tree.leetcode;
 
-import io.ziheng.tree.leetcode.TreeNode;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {
+        // ...
+    }
+    public TreeNode(int val) {
+        this.val = val;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 /**
  * LeetCode 103. Binary Tree Zigzag Level Order Traversal
  * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
  */
 public class BinaryTreeZigzagLevelOrderTraversal {
+    public static void main(String[] args) {
+        // ...
+    }
     private static int IN_ORDER = 1;
     private static int REVERSE_ORDER = -1;
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         if (root == null) {
-            return new LinkedList<List<Integer>>();
+            return new LinkedList<>();
         }
-        List<List<Integer>> resultList = new LinkedList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> rList = new LinkedList<>();
+        Queue<TreeNode> aQueue = new LinkedList<>();
         int order = IN_ORDER;
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            List<Integer> levelList = new LinkedList<>();
-            int size = queue.size();
+        aQueue.offer(root);
+        while (!aQueue.isEmpty()) {
+            List<Integer> aList = new LinkedList<>();
+            int size = aQueue.size();
             for (int i = 0; i < size; i++) {
-                TreeNode currentNode = queue.poll();
-                if (order == REVERSE_ORDER) {
-                    levelList.add(0, currentNode.val);
-                } else if (order == IN_ORDER) {
-                    levelList.add(currentNode.val);
+                TreeNode currentNode = aQueue.poll();
+                if (order == IN_ORDER) {
+                    aList.add(currentNode.val);
+                } else if (order == REVERSE_ORDER) {
+                    aList.add(0, currentNode.val);
                 }
                 if (currentNode.left != null) {
-                    queue.offer(currentNode.left);
+                    aQueue.offer(currentNode.left);
                 }
                 if (currentNode.right != null) {
-                    queue.offer(currentNode.right);
+                    aQueue.offer(currentNode.right);
                 }
             }
+            rList.add(aList);
             order = order == IN_ORDER ? REVERSE_ORDER : IN_ORDER;
-            resultList.add(levelList);
         }
-        return resultList;
+        return rList;
     }
 }
 /* EOF */
